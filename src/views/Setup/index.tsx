8f41f609a522d89eb968a40b95fb1f45921f3475
LukeZekes <zechesl@gmail.com>
2025-05-09 20:17:26 -0500

    Remove MCP setup from setup page

--- src/views/Setup/index.tsx
+++ src/views/Setup/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { useSetAtom } from "jotai"
-import { InterfaceProvider, defaultInterface, mcpConnectionInterface } from "../../atoms/interfaceState"
+import { InterfaceProvider, defaultInterface } from "../../atoms/interfaceState"
 import { useTranslation } from "react-i18next"
 import { useNavigate, useLocation } from "react-router-dom"
 import ModelConfigForm from "./ModelConfigForm"
@@ -54,7 +54,7 @@ const Setup = () => {
         <ModelConfigForm
           provider={localProvider}
           // fields={mcpConnectionInterface}
-          fields={{...defaultInterface[localProvider], ...mcpConnectionInterface}}
+          fields={{...defaultInterface[localProvider]}}
           onSubmit={handleSubmit}
           onProviderChange={setLocalProvider}
           submitLabel="setup.submit"
